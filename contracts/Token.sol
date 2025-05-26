00fd4c3b8381fc8da13a12b0a0d4200210119dc2
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-05-26 15:05:16 +0100

    initializing project

---
+++ contracts/Token.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.20;
+
+contract Token {
+    string public name = "My SU Token";
+    string public symbol = "SU";
+
+    uint public totalSupply = 100000;
+    address public owner;
+
+    mapping(address => uint) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint amount) external {
+        require(balances[msg.sender] >= amount, "not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns(uint) {
+        return balances[account];
+    }
+}
