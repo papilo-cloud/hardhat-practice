953191993ee2d63411ccf1f9b893fd220b2c505f
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-05-27 23:11:53 +0100

    adding the required urls and keys for the network deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-chai-matchers")
 require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    }
+  }
 };
