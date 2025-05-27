204592d828d7932d8430b0edacf992436d917bad
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-05-27 20:47:05 +0100

    debugging using hardhat's console.log statement

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+import "hardhat/console.sol";
 
 contract Token {
     string public name = "My SU Token";
@@ -12,13 +13,24 @@ contract Token {
 
     constructor() {
         balances[msg.sender] = totalSupply;
+        console.log('Owner: %s', msg.sender);
         owner = msg.sender;
     }
 
     function transfer(address to, uint amount) external {
+        console.log(
+            "Trying to send %s tokens from %s to %s",
+            amount,
+            msg.sender,
+            to
+        );
+        console.log("Initial sender balance: %s tokens", balances[msg.sender]);
+        
         require(balances[msg.sender] >= amount, "not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
+
+        console.log("Final sender balance: %s tokens", balances[msg.sender]);
     }
 
     function balanceOf(address account) external view returns(uint) {
