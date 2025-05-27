b77eaa0225d5e15e80303e8f393963b3dbe80c72
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-05-27 23:09:08 +0100

    delete console statements

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "hardhat/console.sol";
 
 contract Token {
     string public name = "My SU Token";
@@ -13,24 +12,13 @@ contract Token {
 
     constructor() {
         balances[msg.sender] = totalSupply;
-        console.log('Owner: %s', msg.sender);
         owner = msg.sender;
     }
 
     function transfer(address to, uint amount) external {
-        console.log(
-            "Trying to send %s tokens from %s to %s",
-            amount,
-            msg.sender,
-            to
-        );
-        console.log("Initial sender balance: %s tokens", balances[msg.sender]);
-        
         require(balances[msg.sender] >= amount, "not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
-
-        console.log("Final sender balance: %s tokens", balances[msg.sender]);
     }
 
     function balanceOf(address account) external view returns(uint) {
